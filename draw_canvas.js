42e0d554551dc2d8438c2f783c1df6562d249823
Franklin Lee <frankgoji@gmail.com>
2015-08-15 00:09:34 -0700

    Commit on Sat Aug 15 00:09:34 PDT 2015

--- draw_canvas.js
+++ draw_canvas.js
@@ -1,1 +1,6 @@
 /* Draws the kanban board and articulates all its elements and objects. */
+// Kanban object and subobjects
+// the board will need multiple divisions
+// events--need elements like title, time, description
+// needs to be interactive, and each interaction should communicate a change to
+// the server
