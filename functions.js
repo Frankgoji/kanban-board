421c1b705bae62935f39ae29802d3b1e5fdeca02
Franklin Lee <frankgoji@gmail.com>
2016-08-24 16:30:16 -0700

    improved the changing columns and others

--- functions.js
+++ functions.js
@@ -64,6 +64,7 @@ function addEvent() {
     xmlhttp.open("POST", "cgi-bin/event_data.py", true);
     xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xmlhttp.send(encodeURI("date="+vals['date']+"&column="+vals['column']+"&title="+vals['title']+"&description="+vals['description']+"&addEvent=True"));
+	is_editing = false;
 }
 
 var orig = '';
@@ -79,7 +80,7 @@ function edit(id) {
     var data = unescape(elem.getAttribute('name'));
     var old_data = data.split('|');
     var col = old_data[0];
-	 old_data[3] = old_data[3].replace(/\<br\>/g, '\n');
+	old_data[3] = old_data[3].replace(/\<br\>/g, '\n');
     var cols = ['do_pool', 'longterm', 'high_priority', 'doing', 'done'];
     var col_names = ['Do Pool', 'Longterm', 'High Priority', 'Doing', 'Done'];
     // create the edit elements
@@ -90,8 +91,7 @@ function edit(id) {
     var date = createElem('input', '', [['id', 'date'], ['type', 'date'], ['name', 'date'], ['value', old_data[1]]]);
     var description = createElem('textarea', old_data[3], [['id', 'description'], ['name', 'description'], ['rows', '2'], ['cols', '30']]);
     var cancel = createElem('input', '', [['type', 'button'], ['value', 'Cancel'], ['onclick', 'getTable()']]);
-    var select = createElem('select', '', [['id', 'change_select'], ['onchange', 'changeColumn("' + data + '")']]);
-    var col = createElem('input', '', [['type', 'button'], ['id', 'column'], ['value', col], ['style', 'display:none']]);
+    var select = createElem('select', '', [['id', 'column'], ['onchange', 'changeEvent("' + data + '")']]);
     select.appendChild(createElem('option', 'Current Column', [['value', col]]));
     for (var i = 0; i < cols.length; i++) {
         if (col != cols[i]) {
@@ -114,12 +114,11 @@ function edit(id) {
     form.appendChild(document.createElement('br'));
     form.appendChild(document.createTextNode('Description:'));
     form.appendChild(description);
-    elem.appendChild(submit);
     form.appendChild(document.createElement('br'));
-    elem.innerHTML += 'Move:';
-    elem.appendChild(select);
-    elem.appendChild(col);
-    elem.appendChild(cancel);
+    form.innerHTML += 'Move:';
+    form.appendChild(select);
+    form.appendChild(submit);
+    form.appendChild(cancel);
 }
 
 /** Edits an event by deleting the old one and adding the new one */
@@ -150,35 +149,12 @@ function deleteEvent(data) {
     is_editing = false;
 }
 
-/** Changes the column of an event */
-function changeColumn(data) {
-    if (window.XMLHttpRequest) {
-        // code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        // code for IE6, IE5
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-            document.getElementById("board").innerHTML = xmlhttp.responseText;
-        }
-    };
-    var newCol = document.getElementById('change_select').value;
-    data = decodeURI(data);
-    data = data.split('|');
-    xmlhttp.open("POST", "cgi-bin/event_data.py", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send(encodeURI("date="+data[1]+"&column="+data[0]+"&title="+data[2]+"&description="+data[3]+"&deleteEvent=True"));
-
-    setTimeout(function(){xmlhttp.open("POST", "cgi-bin/event_data.py", true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send(encodeURI("date="+data[1]+"&column="+newCol+"&title="+data[2]+"&description="+data[3]+"&addEvent=True"));}, 550);
-    is_editing = false;
-}
-
 /** Creates the format to add a cell */
 function addCell(id) {
+    if (is_editing) {
+        return;
+    }
+	is_editing = true;
     var cell = document.getElementById(id);
     var col = id.substring(0, id.length - 'button'.length);
     cell.innerHTML = '';
